f58e77052a264af4a2551010a182cda880c0803a
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-05-26 13:00:24 +0530

    watershed test

--- test.py
+++ test.py
@@ -1,25 +1,29 @@
 import graph
 import time
 import sys
+import tiffile as tf
+from skimage import io
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+from skimage.morphology import watershed
+from scipy.ndimage import label
+from scipy.ndimage.filters import gaussian_filter as gf
+from skimage.util import img_as_float
+from scipy.ndimage.measurements import watershed_ift
 
-N = int(sys.argv[1])
-g = graph.Graph(N)
+slice = 200
+img = tf.imread('../data/image.tif')
+boundary = tf.imread('../data/prob.tif')
 
-T = time.time()
-for i in range(N):
-    g.make_edge(i,(i+1)%N,10*(i+1))
+boundary = img_as_float(boundary)
+boundary = gf(boundary,5)
+raw_input("Enter")
 
-#g.make_edge(0,2,99)
-#g.merge(0,2)
 
-print "Constructing took %f s" % (time.time() - T)
+seed_mask = boundary < 0.01
+seed_label,n = label(seed_mask)
 
-#g.draw('before.png')
+print "Found %d Seeds" % n
+w = watershed(img, seed_label)
 
-for i in range(N-3):
-    g.merge(i,i+1)
-    
-
-print "Merging took %f s" % (time.time() - T)
-#g.draw('graph.png')
-#g.display()
