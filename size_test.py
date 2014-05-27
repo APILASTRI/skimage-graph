b4b1e437cf388c25aa630c82b1087c38e134c1d5
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-05-27 21:58:48 +0530

    pep8 changes

--- size_test.py
+++ size_test.py
@@ -9,13 +9,12 @@ N = int(sys.argv[1])
 D = 10
 
 
-
 g = graph.Graph(N)
 
 for i in range(N):
     for j in range(D):
-        k = random.randint(0,N-1)
-        g.make_edge(i,k,1)
+        k = random.randint(0, N - 1)
+        g.make_edge(i, k, 1)
 
 
 proc = ps.Process(os.getpid())
